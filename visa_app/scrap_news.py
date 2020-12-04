3c9dd3074fa30770e910b50cdb6c654148eaa605
Naitik0909 <parmarnaitik0909@gmail.com>
2020-12-05 01:42:27 +0530

    Scrapping completed

--- visa_app/scrap_news.py
+++ visa_app/scrap_news.py
@@ -5,37 +5,20 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import pprint
 
-class Scrapper():
-	def __init__(self):
-		self.title=
-
-
-def cic_news():
-
-	req = Request('https://www.cicnews.com/latest-immigration-news.html', headers={'User-Agent': 'Mozilla/5.0'})
-	URL = "https://www.cicnews.com/latest-immigration-news.html#gs.n3b9io"
-	html = urllib.request.urlopen(req).read()
-	soup = BeautifulSoup(html, 'html.parser')
-
-	title=[] # a list to store titles
-	description=[]
-	url_images=[]
-	authors=[]
-	final_description=[]
-	urls=[]
-
-
+def get_title(soup):
+	titles = []
 	count=0
 	for div in soup.findAll('span',
 							attrs = {'class':'entry-title-primary'}):
 		count+=1
 		if(count<6):
-			title.append(div.text.strip())
+			titles.append(div.text.strip())
 		else:
 			break
+	return titles
 
-
-
+def get_combined(soup):
+	description = []
 	count1=0
 	for div in soup.findAll('div',
 							attrs = {'class':'text'}):
@@ -45,11 +28,26 @@ def cic_news():
 		else:
 			break
 
+	return description
+
+
+def get_author(description, soup):
+	authors = []
+	for author in description:
+		temp=author.split('\n\n')
+		authors.append(temp[1])
+	return authors
+
+
+def get_description(description, soup):
+	final_description = []
 	for author in description:
 		temp=author.split('\n\n')
 		final_description.append(temp[0])
-		authors.append(temp[1])
+	return final_description
 
+def get_url(soup):
+	urls = []
 	found=False
 	counter=0
 	for a in soup.findAll('a', href=True):
@@ -60,9 +58,10 @@ def cic_news():
 
 		if(a['href']=='https://www.cicnews.com'):
 			found=True
+	return urls
 
-
-
+def get_url_images(soup):
+	url_images = []
 	counter1=0
 	images=soup.findAll('img')
 	for image in images:
@@ -70,29 +69,28 @@ def cic_news():
 		if(counter1>5 and counter1<11):
 			url_images.append(image['src'])
 
+	return url_images
 
 
-	pprint.pprint(authors)
-	print('\n\n')
-	pprint.pprint(urls)
-	print('\n\n')
-	pprint.pprint(title)
-	print('\n\n')
-	print(final_description)
-	print('\n\n')
-	pprint.pprint(url_images)
-	print('\n\n')
 
-for i in range(0,5):
-	news = models.News.objects.create(
-		author=authors[i],
-		title=title[i],
-		description=final_description[i],
-		url=urls[i]
-	)
-	news.save()
 
-cic_news()
+class Scrapper():
+	def __init__(self):
+		req = Request('https://www.cicnews.com/latest-immigration-news.html', headers={'User-Agent': 'Mozilla/5.0'})
+		URL = "https://www.cicnews.com/latest-immigration-news.html#gs.n3b9io"
+		html = urllib.request.urlopen(req).read()
+		soup = BeautifulSoup(html, 'html.parser')
+
+		self.titles = get_title(soup)
+		self.combined = get_combined(soup)
+		self.authors = get_author(self.combined, soup)
+		self.descriptions = get_description(self.combined, soup)
+		self.urls = get_url(soup)
+		self.url_images = get_url_images(soup)
+
+
+# urllib.request.urlretrieve("http://www.gunnerkrigg.com//comics/00000001.jpg", "00000001.jpg")
+
 # TODO: SCRAP CANADA.CA WEBSITE---------------------------------->
 # def canada_ca(title):
 # req = Request('https://www.canada.ca/en/news.html')
